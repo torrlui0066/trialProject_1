372ee61f6d325502ff79105643df582a4e99d95c
torrlui0066 <torrlui0066@gmail.com>
2024-02-21 16:42:03 -0600

    PlayerCreatedw/Animations
    - Added Character Sprite
    - Added Animation Spirte Sheets
    - Added animations for:
       Run, Jump, Fall, Attack
    - Added hurtbox to Attack
    - Added hitbox to Character

--- Player/player(scene).gd
+++ Player/player(scene).gd
@@ -7,22 +7,56 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = get_node("AnimationPlayer")
+
+var isAttacking: bool = false
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		anim.play("Jump")
+		
+	if Input.is_action_just_pressed("attack"):
+		isAttacking = true
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false
 	if direction:
 		velocity.x = direction * SPEED
+		if isAttacking == false:
+			if velocity.y == 0:
+				anim.play("Run")
+		elif isAttacking == true:
+				anim.play("Attack")
+				isAttacking = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		#if isAttacking == false:
+		if velocity.y == 0:
+			if isAttacking == false:
+				anim.play("Idle")
+			elif isAttacking == true:
+				anim.play("Attack")
+				await anim.animation_finished
+				isAttacking = false
+			#isAttacking = false
+	if velocity.y > 0:
+		anim.play("Fall")
+		
+	#isAttacking = false
 	move_and_slide()
+	
+		
+func _on_sword_swing_area_entered(area):
+	if area.is_in_group("hurtbox"):
+		area.take_damage()
