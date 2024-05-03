8387e5e005dae744dcffcb398fdf637d638395ec
ArTobi <145605629+ArTobi@users.noreply.github.com>
2024-05-03 15:06:32 -0500

    on-site changes for presentation
    Random changes inside the final presentation.

--- Player/GUI/player_data.gd
+++ Player/GUI/player_data.gd
@@ -1,13 +1,13 @@
 extends Node2D
 class_name player_data
 
-static var coin = 60
+static var coin = 0
 static var life = 4
 static var max_life = 4
 static var kills = 0
 static var sword_damage = 1
 static var fireball_damage = 1
-static var boss = 1
+static var boss = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
